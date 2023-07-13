3818ae2066d6de1d4446a3a607f44f8436d0132c
Weike Qu <weikequ@amazon.com>
2023-07-13 15:39:19 -0700

    Updated db to use ferretdb
    Signed-off-by: Weike Qu <weikequ@amazon.com>

--- backend/db/index.js
+++ backend/db/index.js
@@ -12,15 +12,15 @@ exports.connect = (app) => {
 
   const connectWithRetry = () => {
     mongoose.Promise = global.Promise;
-    console.log("MongoDB connection with retry");
+    console.log("DB connection with retry");
     mongoose
       .connect(process.env.MONGODB_URI, options)
       .then(() => {
-        console.log("MongoDB is connected");
+        console.log("FerretDB is connected");
         app.emit("ready");
       })
       .catch((err) => {
-        console.log("MongoDB connection unsuccessful, retry after 2 seconds.", err);
+        console.log("DB connection unsuccessful, retry after 2 seconds.", err);
         setTimeout(connectWithRetry, 2000);
       });
   };
